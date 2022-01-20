f6ac62963eed5be70bbd629d41e41eb1676638f4
jtiagosantos <tiago.santos@icomp.ufam.edu.br>
2022-01-20 19:29:01 -0300

    chore: add header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,13 @@
+import Link from 'next/link';
+
+import styles from './header.module.scss';
+
 export default function Header() {
-  // TODO
+  return (
+    <div className={styles.header_container}>
+      <Link href="/">
+        <img src="/Logo.svg" alt="logo" />
+      </Link>
+    </div>
+  );
 }
