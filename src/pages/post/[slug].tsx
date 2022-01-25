6454920c9062ad4b930e2cb3b678aa24a7622f1b
jtiagosantos <tiago.santos@icomp.ufam.edu.br>
2022-01-25 07:13:21 -0300

    chore: add post page

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,6 +1,11 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
-
+import { useRouter } from 'next/router';
+import Link from 'next/link';
+import { FaCalendarAlt, FaUserAlt, FaRegClock } from 'react-icons/fa';
+import PrismicDOM from 'prismic-dom';
 import { getPrismicClient } from '../../services/prismic';
+
+import { calculateRedingTime } from '../../utils/calculateReadingTime';
 
 import commonStyles from '../../styles/common.module.scss';
 import styles from './post.module.scss';
@@ -26,20 +31,94 @@ interface PostProps {
   post: Post;
 }
 
-// export default function Post() {
-//   // TODO
-// }
+export default function Post({ post }: PostProps) {
+  const router = useRouter();
 
-// export const getStaticPaths = async () => {
-//   const prismic = getPrismicClient();
-//   const posts = await prismic.query(TODO);
+  const formattedPost = {
+    ...post,
+    time: calculateRedingTime(post.data.content),
+    data: {
+      ...post.data,
+      content: {
+        heading: post.data.content.map(data => data.heading),
+        body: post.data.content.map(data =>
+          PrismicDOM.RichText.asText(data.body)
+        ),
+      },
+    },
+  };
 
-//   // TODO
-// };
+  if (router.isFallback) {
+    return <div>Carregando...</div>;
+  }
 
-// export const getStaticProps = async context => {
-//   const prismic = getPrismicClient();
-//   const response = await prismic.getByUID(TODO);
+  return (
+    <div className={styles.post_container}>
+      <header>
+        <Link href="/">
+          <img src="/Logo.svg" alt="logo" />
+        </Link>
+      </header>
 
-//   // TODO
-// };
+      <main>
+        <div className={styles.banner_container}>
+          <img src={post.data.banner.url} alt={post.data.title} />
+        </div>
+
+        <section className={styles.content}>
+          <h1 className={styles.title}>{post.data.title}</h1>
+
+          <div className={styles.info}>
+            <p>
+              <FaCalendarAlt />
+              {post.first_publication_date}
+            </p>
+            <p>
+              <FaUserAlt />
+              {post.data.author}
+            </p>
+            <p>
+              <FaRegClock />
+              {formattedPost.time} min
+            </p>
+          </div>
+
+          <div className={styles.post_text}>
+            {formattedPost.data.content.heading.map((data, index) => (
+              <article>
+                <h1 className={styles.heading}>{data}</h1>
+                <p className={styles.body}>
+                  {formattedPost.data.content.body[index]}
+                </p>
+              </article>
+            ))}
+          </div>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [
+      '/post/axios-e-um-cliente-http-baseado-em-promises-para-fazer',
+      '/post/introducao-a-testing-library--testando-componentes',
+      '/post/jquery-a-historia-da-biblioteca-js-mais-usada-da-ultima',
+    ],
+    fallback: true,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const { slug } = params;
+
+  const prismic = getPrismicClient();
+  const response = await prismic.getByUID('post', String(slug), {});
+
+  const post = { ...response };
+
+  return {
+    props: { post },
+  };
+};
